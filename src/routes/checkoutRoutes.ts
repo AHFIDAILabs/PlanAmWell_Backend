c0614b6f29f4a8c2a893515555717eb4c3d1f108
zionnaire <zionnaire2018@gmail.com>
2025-12-06 12:36:54 -0800

    updating checkoutRoutes to include verifyToken

--- src/routes/checkoutRoutes.ts
+++ src/routes/checkoutRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";   
 import { checkout } from "../controllers/checkoutController"; 
-import { guestAuth } from "../middleware/auth";
+import { guestAuth, verifyToken } from "../middleware/auth";
 
 const checkoutRouter = Router();
 
 // Public - anyone can initiate checkout
-checkoutRouter.post("/", guestAuth, checkout);
+checkoutRouter.post("/", guestAuth, verifyToken,  checkout);
 
 export default checkoutRouter;
