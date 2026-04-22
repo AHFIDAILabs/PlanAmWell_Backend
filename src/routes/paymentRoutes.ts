6d377b8660281cd13b79ee1014dbd2fafccfd5de
zionnaire <zionnaire2018@gmail.com>
2026-04-22 15:38:55 +0100

    Updates: order

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { getPaymentMethods, initiatePayment,   paymentRedirect, getPaymentByOrder
+import { getPaymentMethods, initiatePayment,   paymentRedirect, getPaymentByOrder, verifyPayment
  } from "../controllers/paymentController";
 import { guestAuth } from "../middleware/auth";
 
@@ -9,8 +9,11 @@ const paymentRouter = Router();
 // Allow guests to access payment methods and initiate payment
 paymentRouter.get("/methods", guestAuth, getPaymentMethods);
 paymentRouter.post("/initiate", guestAuth, initiatePayment);
+paymentRouter.post("/verify", guestAuth, verifyPayment); 
 paymentRouter.get("/redirect", guestAuth, paymentRedirect);
 paymentRouter.get("/by-order/:orderId", guestAuth, getPaymentByOrder);
+
+
 
 
 export default paymentRouter;
